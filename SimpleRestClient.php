10d597f6b489b0ed2f094ad4d1016bc3393ccc98
dgrundel <daniel@webpresencepartners.com>
2015-03-04 13:18:28 -0500

    finish of first draft

--- SimpleRestClient.php
+++ SimpleRestClient.php
@@ -7,6 +7,7 @@ class SimpleRestClient {
 	private $password;
 	private $host;
 	private $connection = null;
+	private $idColumnName = 'id';
 
 	public function __construct($dbname, $dbtable, $username = '', $password = '', $host = 'localhost') {
 		$this->dbname = $dbname;
@@ -14,6 +15,31 @@ class SimpleRestClient {
 		$this->username = $username;
 		$this->password = $password;
 		$this->host = $host;
+	}
+
+	public function init() {
+		$http_verb = strtolower( isset($_REQUEST['_method']) ? $_REQUEST['_method'] : $_SERVER['REQUEST_METHOD'] );
+		$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
+		$data = isset($_REQUEST['data']) ? $_REQUEST['data'] : null;
+		
+		switch($http_verb) {
+			case 'post':
+				$this->store($data);
+				break;
+			case 'put':
+				$this->update($id, $data);
+				break;
+			case 'delete':
+				$this->delete($id);
+				break;
+			case 'get':
+			default:
+				if(!empty($id)) {
+					$this->show($id);
+				} else {
+					$this->index();
+				}
+		}
 	}
 
 	public function getConnection() {
@@ -28,6 +54,44 @@ class SimpleRestClient {
 		return $this->connection;
 	}
 
+	public function setIdColumnName($name) {
+		$this->idColumnName = $name;
+	}
+
+	public function escapeId($id) {
+		$id = intval($id);
+		if($id > 0) {
+			return $id;
+		} else {
+			throw new Exception("Bad id value.");
+		}
+	}
+
+	public function escapeValue($value) {
+		if(is_numeric($value)) {
+			$int_value = intval($value);
+			$float_value = floatval($value);
+			return $int_value != $float_value ? $float_value : $int_value;
+		} else {
+			return "'" . $this->getConnection()->real_escape_string($value) . "'";
+		}
+	}
+
+	public function escapeRecord($record) {
+		$columns = array_keys($record);
+		$columns = array_map(function($column){
+			return $this->getConnection()->real_escape_string($column);
+		}, $columns);
+		
+		$values = array_values($record);
+		$values = array_map(array($this, 'escapeValue'), $values);
+
+		return array(
+			'columns' => $columns,
+			'values' => $values,
+		);
+	}
+
 	public function makeJsonResponse($data) {
 		header('Content-Type: application/json');
 		echo json_encode($data);
@@ -36,51 +100,90 @@ class SimpleRestClient {
 
 	public function index() {
 		$result = $this->getConnection()->query("SELECT * FROM {$this->dbtable}");
-		$records = array();
-		if($result) {
+		if($result === false) {
+			$this->makeJsonResponse(array(
+				'error' => $this->getConnection()->error
+			));
+		} else {
+			$records = array();
 			while ($row = $result->fetch_assoc()) {
 				$records[] = $row;
 			}
+			$this->makeJsonResponse($records);
 		}
-		$this->makeJsonResponse($records);
+	}
+
+	public function show($id) {
+		$id = $this->escapeId($id);
+		$result = $this->getConnection()->query("SELECT * FROM {$this->dbtable} WHERE {$this->idColumnName} = {$id} LIMIT 1");
+		if($result === false) {
+			$this->makeJsonResponse(array(
+				'error' => $this->getConnection()->error
+			));
+		} else {
+			$record = null;
+			if($result && $row = $result->fetch_assoc()) {
+				$record = $row;
+			}
+			$this->makeJsonResponse($record);
+		}
 	}
 
 	public function store($record) {
-		$connection = $this->getConnection();
-		$columns = implode(',', array_keys($record));
-		$values = "'" . implode(',', array_map(array($connection, 'real_escape_string'), array_values($record))) . "'";
+		$escaped = $this->escapeRecord($record);
+		$column_string = implode(',', $escaped['columns']);
+		$value_string = implode(',', $escaped['values']);
 
-		$result = $connection->query("INSERT INTO {$this->dbtable} ({$columns}) VALUES ({$values})");
+		$result = $this->getConnection()->query("INSERT INTO {$this->dbtable} ({$column_string}) VALUES ({$value_string})");
 
-		// $sql="INSERT INTO tbl (col1_varchar, col2_number) VALUES ($v1,10)";
-
-		// if($conn->query($sql) === false) {
-		// 	trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-		// } else {
-		// 	$last_inserted_id = $conn->insert_id;
-		// 	$affected_rows = $conn->affected_rows;
-		// }
+		if($result === false) {
+			$this->makeJsonResponse(array(
+				'error' => $this->getConnection()->error
+			));
+		} else {
+			$this->makeJsonResponse(array(
+				'insertId' => $this->getConnection()->insert_id
+			));
+		}
 	}
 
 	public function update($id, $record) {
-		// $v1="'" . $conn->real_escape_string('col1_value') . "'";
+		$id = $this->escapeId($id);
+		$escaped = $this->escapeRecord($record);
 
-		// $sql="UPDATE tbl SET col1_varchar=$v1, col2_number=1 WHERE id>10";
+		$escaped_record = array_combine($escaped['columns'], $escaped['values']);
+		$update_values = array();
+		foreach ($escaped_record as $column => $value) {
+			$update_values[] = "{$column} = {$value}";
+		}
+		$update_string = implode(',', $update_values);
 
-		// if($conn->query($sql) === false) {
-		// trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-		// } else {
-		// $affected_rows = $conn->affected_rows;
-		// }
+		$result = $this->getConnection()->query("UPDATE {$this->dbtable} SET {$update_string} WHERE {$this->idColumnName} = {$id} LIMIT 1");
+		
+		if($result === false) {
+			$this->makeJsonResponse(array(
+				'error' => $this->getConnection()->error
+			));
+		} else {
+			$this->makeJsonResponse(array(
+				'affectedRows' => $this->getConnection()->affected_rows
+			));
+		}
 	}
 
 	public function delete($id) {
-		// $sql="DELETE FROM tbl WHERE id>10";
+		$id = $this->escapeId($id);
 
-		// if($conn->query($sql) === false) {
-		// trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-		// } else {
-		// $affected_rows = $conn->affected_rows;
-		// }
+		$result = $this->getConnection()->query("DELETE FROM {$this->dbtable} WHERE {$this->idColumnName} = {$id} LIMIT 1");
+		
+		if($result === false) {
+			$this->makeJsonResponse(array(
+				'error' => $this->getConnection()->error
+			));
+		} else {
+			$this->makeJsonResponse(array(
+				'affectedRows' => $this->getConnection()->affected_rows
+			));
+		}
 	}
 }
--- demo.php
+++ demo.php
@@ -3,4 +3,5 @@
 require('SimpleRestClient.php');
 
 $client = new SimpleRestClient('mysql', 'help_category', 'root');
-$client->index();
+$client->setIdColumnName('help_category_id');
+$client->init();
